4c31bf9f3880ba68d9c3997a411f87392925d240
Erik Olsen <erik.olsen@milespartnership.com>
2020-10-17 20:11:54 -0400

    prev and next in projects

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -16,15 +16,29 @@ export default {
       error({ statusCode: 404, message: 'Page not found', res: err })
     }
     const project = await $content('projects', slug).fetch().catch(reportErr)
-    const projects = await $content('projects').fetch().catch(reportErr)
+    let [prev, next] = await $content('projects')
+      .only(['title', 'slug'])
+      .sortBy('date', 'desc')
+      .surround(params.slug)
+      .fetch()
+    const projects = await $content('projects')
+      .only(['title', 'slug'])
+      .where({ slug: { $ne: 'index' } })
+      .sortBy('date', 'desc')
+      .fetch()
+      .catch(reportErr)
 
     const meta = {
       title: project.title,
       desc: project.description,
     }
+    prev = prev || projects[projects.length - 1]
+    next = next || projects[0]
     return {
       project,
       projects,
+      prev,
+      next,
       meta,
     }
   },

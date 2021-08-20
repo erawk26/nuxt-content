9f78ddedebef0be551d1fb5503a95ee79edab121
Erik Olsen <erik.olsen@milespartnership.com>
2021-08-20 00:29:08 -0400

    set front page template

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+section.page
+  h1 {{ page.title }}
+  p {{ page.description }}
+  nuxt-content(:document='page')
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    const slug = 'index'
+    const reportErr = (err) => {
+      error({ statusCode: 404, message: 'Page not found', res: err })
+    }
+    const page = await $content(slug).fetch().catch(reportErr)
+
+    const meta = {
+      title: page.title,
+      desc: page.description,
+    }
+    return {
+      page,
+      meta,
+    }
+  },
+}
+</script>

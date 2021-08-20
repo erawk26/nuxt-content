3c38f170bf756a5563ebc7e248e88a9c139337a4
Erik Olsen <erik.olsen@milespartnership.com>
2021-08-20 00:27:48 -0400

    add aria label to theme toggle button

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,10 +1,22 @@
 <template lang="pug">
-  span
-    v-tooltip(allow-overflow right :position-y="150" :position-x="350" v-model="showTooltip")
-      template(#activator="showTooltip")
-        v-btn.dark-btn(link icon @mouseenter="flashTooltip()" @click="cycleTheme()")
-          v-icon {{colorMode==='auto'?'mdi-brightness-auto':$vuetify.theme.dark?'mdi-brightness-5':'mdi-brightness-4'}}
-      span {{colorMode==='auto'?'Auto':$vuetify.theme.dark?'Dark':'Light'}} Mode
+span
+  v-tooltip(
+    allow-overflow,
+    right,
+    :position-y='150',
+    :position-x='350',
+    v-model='showTooltip'
+  )
+    template(#activator='showTooltip')
+      v-btn.dark-btn(
+        :aria-label='`set theme to ${getNextColorMode()} mode`',
+        link,
+        icon,
+        @mouseenter='flashTooltip()',
+        @click='cycleTheme()'
+      )
+        v-icon {{ colorMode === "auto" ? "mdi-brightness-auto" : $vuetify.theme.dark ? "mdi-brightness-3" : "mdi-brightness-5" }}
+    span {{ colorMode === "auto" ? "Auto" : $vuetify.theme.dark ? "Dark" : "Light" }} Mode
 </template>
 
 <script>
@@ -40,15 +52,14 @@ export default {
         this.showTooltip = false
       }, time)
     },
-    getNextColorMode(color) {
-      // just cycle the different modes, not changing color in here
-      const nextColor =
-        color === 'auto' ? 'light' : color === 'light' ? 'dark' : 'auto'
-      return nextColor
+    getNextColorMode(color = this.colorMode) {
+      const arr = ['light', 'dark', 'auto']
+      const i = arr.indexOf(color)
+      return arr[this.looper(arr, i)]
     },
     cycleTheme() {
       // set colormode var, set theme
-      this.colorMode = this.getNextColorMode(this.colorMode)
+      this.colorMode = this.getNextColorMode()
       this.colorMode === 'auto'
         ? this.changeTheme(this.isNight)
         : this.colorMode === 'dark'
